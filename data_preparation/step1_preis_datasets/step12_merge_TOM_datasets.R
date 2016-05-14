3aef8ddcd198f8d491d575924d82d4885a0b4eb4
nvkov <nikoleta_kovachka@abv.bg>
2016-05-14 19:11:49 +0200

    eur pln

--- data_preparation/step1_preis_datasets/step12_merge_TOM_datasets.R
+++ data_preparation/step1_preis_datasets/step12_merge_TOM_datasets.R
@@ -6,3 +6,15 @@ setwd(paste0(project_directory, data_directory))
 
 load("TOMmerge.RData")
 
+tompln<- tomdf[tomdf$Bemerkung=="PLN" | tomdf$Bemerkung=="PLN, NEGOTIABLE" | tomdf$Bemerkung=="PLN, Netto" ]
+
+rm(tomdf)
+eurpln<- read.csv("eurpln.txt", sep=";")
+eurpln$Date<- as.Date(eurpln$Date)
+
+meanXchange<- function(eurpln, firstDate, lastDate){
+  mean(eurpln$Price[eurpln$Date>firstDate & eurpln$Date<lastDate])
+}
+
+meanXchange(eurpln, "2010-11-17", "2011-12-18")
+meanXchange(eurpln, tompln$firstDate, tompln$lastDate)
